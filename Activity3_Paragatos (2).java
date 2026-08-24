27db99523f014da69631d4b7ef21e0c77e2b10fe
paragatosmariss-sketch <paragatosmariss@gmail.com>
2026-08-24 21:42:04 +0800

    Delete Activity3_Paragatos (2).java

--- Activity3_Paragatos (2).java
+++
@@ -1,9 +0,0 @@
-public class Activity3_Paragatos {
-    public static void main(String[] args) {
-        int score = 80;
-
-        String result = score >= 75 ? "Passed" : "Failed";
-
-        System.out.println("Result: " + result);
-    }
-}
